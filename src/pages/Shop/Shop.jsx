8c76cccf33881e4291ef9895bbcb07b72ddf2434
http1224com <ben233n@gmail.com>
2025-03-22 00:49:22 +0800

    322完成第一頁準備第二頁

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Head from '../../components/Head/Head'
 import Foot from '../../components/Foot/Foot'
 import Air from '../../components/Air/Air'
+import Store from '../../components/Store/Store'
 
 const Shop = () => {
   return (
     <>
         <Head/>
         <Air/>
+        <Store/>
         <Foot/>
     </>
   )
